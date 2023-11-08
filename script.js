8dc3f92b5447f4960eba7e9bdb572f028bea263c
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-11-08 10:24:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Function 1: Display an alert
+// Function 1: Alert
 function showAlert() {
-    alert("Hello, world!");
+    alert("Welcome to my website!");
 }
 
 // Function 2: Reverse Text
@@ -11,7 +11,7 @@ function reverseText() {
 }
 
 
-// Function 3: Calculate the sum of two numbers
+// Function 3: Sum
 function calculateSum() {
     const num1 = parseFloat(document.getElementById("num1").value) || 0;
     const num2 = parseFloat(document.getElementById("num2").value) || 0;
@@ -19,26 +19,26 @@ function calculateSum() {
     document.getElementById("sumResult").textContent = `Sum: ${sum}`;
 }
 
-// Function 4: Toggle an element's visibility
+// Function 4: Toggle
 function toggleVisibility() {
     const element = document.getElementById("toggleMe");
     element.style.display = (element.style.display === "none") ? "block" : "none";
 }
 
-// Function 5: Get the current date and time
+// Function 5: Date and Time
 function getCurrentDateTime() {
     const now = new Date();
     document.getElementById("dateTimeResult").textContent = now.toLocaleString();
 }
 
-// Function 6: Clear input fields
+// Function 6: Clear Inputs
 function clearInputs() {
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
     document.getElementById("textToChange").value = "";
 }
 
-// Function 7: Display an alert with a custom message
+// Function 7: Custom Message
 function showCustomAlert() {
     const message = prompt("Enter a custom message:");
     if (message) {
